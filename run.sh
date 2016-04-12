097f56901a43fdc974a6109e16540d9c3110574e
James Lademann <james.lademann@landregistry.gsi.gov.uk>
2016-04-12 11:20:47 +0100

    Add Dockerfile and change build/test/run to use docker
    Add Dockerfile and change build/test/run to use docker for development
    usage (other envs will need different run commands).  Added a few things
    into gitignore.

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
-export SETTINGS="application.config.DevelopmentConfig"
+#!/bin/bash
 
-python run.py
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+[ -z "$SETTINGS" ] && export SETTINGS="application.config.DevelopmentConfig"
+
+[ -z "$PORT" ] && export PORT=5001
+
+exec docker run --name llc-api --rm -e PORT -e SETTINGS -v ${dir}:/srv/llc-api -p $PORT:$PORT llc-api
--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
-export SETTINGS="config.DevelopmentConfig"
+#!/bin/bash
 
-py.test --junitxml=TEST-flask-small-app.xml --cov-report term-missing --cov application tests
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+[ -z "$SETTINGS" ] && export SETTINGS="application.config.TestConfig"
+
+[ -z "$PORT" ] && export PORT=5001
+
+test_command="py.test --junitxml=TEST-flask-small-app.xml --cov-report term-missing --cov application tests"
+
+docker run --name llc-api-test --rm -e PORT -e SETTINGS -v ${dir}:/srv/llc-api llc-api $test_command
