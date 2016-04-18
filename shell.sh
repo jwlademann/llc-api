d5cb1db776f804028e5a7bf777161c29daec266c
James Lademann <james.lademann@landregistry.gsi.gov.uk>
2016-04-18 16:04:50 +0100

    Add shell script

---
+++ shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+[ -z "$SETTINGS" ] && export SETTINGS="application.config.DevelopmentConfig"
+
+[ -z "$PORT" ] && export PORT=5001
+
+docker run -it --name llc-api-test --rm -e PORT -e SETTINGS -v ${dir}:/srv/llc-api llc-api /bin/bash
