b5f6e337c78f793b464461da71d9c12812f4a80a
Aron Jasper <aron.jasper@digital.landregistry.gov.uk>
2016-05-12 15:54:08 +0100

    Fix validation code

--- application/charge_utils.py
+++ application/charge_utils.py
@@ -1,12 +1,13 @@
 from application.validators import ChargeSchema, FurtherInfoSchema, AuthoritySchema, ProvisionSchema
 from application import app
 import requests
+from flask import jsonify
 
 register_details = {
-    "local-land-charge": {"validator": ChargeSchema, "url_parameter": "local-land-charge"},
-    "further-information": {"validator": FurtherInfoSchema, "url_parameter": "further-information"},
-    "originating-authority": {"validator": AuthoritySchema, "url_parameter": "originating-authority"},
-    "statutory-provision": {"validator": ProvisionSchema, "url_parameter": "statutory-provision"}
+    "local-land-charge": {"validator": ChargeSchema(), "url_parameter": "local-land-charge"},
+    "lc-place-of-inspection": {"validator": FurtherInfoSchema(), "url_parameter": "lc-place-of-inspection"},
+    "llc-registering-authority": {"validator": AuthoritySchema(), "url_parameter": "llc-registering-authority"},
+    "statutory-provision": {"validator": ProvisionSchema(), "url_parameter": "statutory-provision"}
 }
 
 def update_charge():
@@ -14,13 +15,17 @@ def update_charge():
 
 def create_charge(request):
     subdomain = get_subdomain(request)
-    result = register_details[subdomain]['validator'].loads(request.get_json())
-    if result.errors:
-        return result.errors
+    if subdomain in register_details:
+        result = register_details[subdomain]['validator'].load(request.get_json())
+        if result.errors:
+            return jsonify(result.errors)
+        else:
+            requests.post(app.config['LLC_REGISTER_URL'] + "/" +
+                          register_details[subdomain]['url_parameter'] + "/items",
+                          data=request.get_json())
+        return 'Charge created'
     else:
-        requests.post(app.config['LLC_REGISTER_URL'] + "/" +
-                      register_details[subdomain]['url_parameter'] + "/items",
-                      data=request.get_json())
+        return 'invalid sub-domain'
 
 def get_subdomain(request):
     return request.headers['Host'].split('.')[0]
--- application/validators.py
+++ application/validators.py
@@ -1,17 +1,17 @@
 from marshmallow import Schema, fields
 
 class ChargeSchema(Schema):
-    provision_id = fields.Integer(required=True)
-    creation_date = fields.Date(required=True)
-    charge_type = fields.String(required=True)
-    geometry_description = fields.String(required=True)
-    geometry = fields.String(required=True)
+    provision_id = fields.String(required=True, attribute="provision")
+    charge_type = fields.String(required=True, attribute="charge-type")
+    geometry_description = fields.String(required=True, attribute="description")
+    geometry = fields.String(required=True, attribute="geometry")
+    originating_authority_id = fields.String(required=True, attribute="originating-authority")
 
 class FurtherInfoSchema(Schema):
-    location = fields.String(required=True)
+    location = fields.String(required=True, attribute="location")
 
 class AuthoritySchema(Schema):
-    name = fields.String(required=True)
+    name = fields.String(required=True, attribute="name")
 
 class ProvisionSchema(Schema):
-    description = fields.String(required=True)
+    description = fields.String(required=True, attribute="description")
