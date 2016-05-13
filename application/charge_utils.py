80d43e96bc42df8ff2c043b842a211add613cb75
LlamaComedian <LlamaComedian@github.com>
2016-05-13 08:52:35 +0100

    added code to remove non-mandatory fields from inbound json

--- application/charge_utils.py
+++ application/charge_utils.py
@@ -1,4 +1,4 @@
-from application.validators import ChargeSchema, FurtherInfoSchema, AuthoritySchema, ProvisionSchema
+from application.validators import ChargeSchema, PlaceOfInspectionSchema, AuthoritySchema, ProvisionSchema
 from application import app
 import requests
 from flask import jsonify
@@ -10,17 +10,22 @@ register_details = {
     "statutory-provision": {"validator": ProvisionSchema(), "url_parameter": "statutory-provision"}
 }
 
+valid_keys=["provision","charge-type","description","geometry","originating-authority","authority-charge-id",
+            "creation-date","expiration-date","instrument","migrating-authority","old-register-part",
+            "place-of-inspection","inspection-reference"
+            ]
+
 def update_charge():
     return 'Charge updated'
 
 def create_charge(request):
     subdomain = get_subdomain(request)
     if subdomain in register_details:
-        result = register_details[subdomain]['validator'].load(request.get_json())
+        json_in = request.get_json()
+        result = register_details[subdomain]['validator'].load(json_in)
         if result.errors:
             return jsonify(result.errors)
         else:
-            json_in = request.get_json()
             json_new = remove_excess_fields(json_in)
             requests.post(app.config['LLC_REGISTER_URL'] + "/" +
                           register_details[subdomain]['url_parameter'] + "/items",
@@ -32,5 +37,11 @@ def create_charge(request):
 def get_subdomain(request):
     return request.headers['Host'].split('.')[0]
 
+#remove any fields from the incoming json that are not in the spec
 def remove_excess_fields(json_in):
-    return json_in
+    json_new = {}
+    for key in json_in:
+        if key in valid_keys:
+            json_new[key] = json_in[key]
+
+    return json_new
