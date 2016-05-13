a8d15424c6206ce53e5b9715689b1ac5db6005fe
LlamaComedian <LlamaComedian@github.com>
2016-05-13 15:05:06 +0100

    added code to remove elements from incoming json which are not required

--- application/charge_utils.py
+++ application/charge_utils.py
@@ -10,10 +10,15 @@ register_details = {
     "statutory-provision": {"validator": ProvisionSchema(), "url_parameter": "statutory-provision"}
 }
 
-valid_keys=["provision","charge-type","description","geometry","originating-authority","authority-charge-id",
+valid_keys={
+    "local-land-charge": ["provision","charge-type","description","geometry","originating-authority","authority-charge-id",
             "creation-date","expiration-date","instrument","migrating-authority","old-register-part",
             "place-of-inspection","inspection-reference"
-            ]
+            ],
+    "llc-place-of-inspection": ["location"],
+    "llc-registering-authority": ["name"],
+    "statutory-provision": ["description"]
+}
 
 def update_charge():
     return 'Charge updated'
@@ -22,11 +27,11 @@ def create_charge(request):
     subdomain = get_subdomain(request)
     if subdomain in register_details:
         json_in = request.get_json()
-        result = register_details[subdomain]['validator'].load(json_in)
-        if result.errors:
-            return jsonify(result.errors)
+        errors = register_details[subdomain]['validator'].validate(json_in)
+        if errors:
+            return jsonify(errors)
         else:
-            json_new = remove_excess_fields(json_in)
+            json_new = remove_excess_fields(json_in, subdomain)
             requests.post(app.config['LLC_REGISTER_URL'] + "/" +
                           register_details[subdomain]['url_parameter'] + "/items",
                           data=json_new)
@@ -37,11 +42,11 @@ def create_charge(request):
 def get_subdomain(request):
     return request.headers['Host'].split('.')[0]
 
-#remove any fields from the incoming json that are not in the spec
-def remove_excess_fields(json_in):
+def remove_excess_fields(json_in, subdomain):
+    """remove any fields from the incoming json that are not in the spec"""
     json_new = {}
     for key in json_in:
-        if key in valid_keys:
+        if key in valid_keys[subdomain]:
             json_new[key] = json_in[key]
 
     return json_new
--- application/validators.py
+++ application/validators.py
@@ -1,26 +1,26 @@
 from marshmallow import Schema, fields
 
 class ChargeSchema(Schema):
-    provision_id = fields.String(required=True, attribute="provision")
-    charge_type = fields.String(required=True, attribute="charge-type")
-    geometry_description = fields.String(required=True, attribute="description")
-    geometry = fields.String(required=True, attribute="geometry")
-    originating_authority_id = fields.String(required=True, attribute="originating-authority")
+    provision_id = fields.String(required=True, load_from="provision")
+    charge_type = fields.String(required=True, load_from="charge-type")
+    geometry_description = fields.String(required=True, load_from="description")
+    geometry = fields.String(required=True, load_from="geometry")
+    originating_authority_id = fields.String(required=True, load_from="originating-authority")
 
-    authority_charge_id = fields.String(required=False, attribute="authority-charge-id")
-    creation_date = fields.Date(required=False, attribute="creation-date")
-    expiration_date = fields.Date(required=False, attribute="expiration-date")
-    instrument = fields.String(required=False, attribute="instrument")
-    migrating_authority = fields.String(required=False, attribute="migrating-authority")
-    old_register_part = fields.String(required=False, attribute="old-register-part")
-    place_of_inspection = fields.String(required=False, attribute="place-of-inspection")
-    inspection_reference = fields.String(required=False, attribute="inspection-reference")
+    authority_charge_id = fields.String(required=False, load_from="authority-charge-id")
+    creation_date = fields.String(required=False, load_from="creation-date")
+    expiration_date = fields.String(required=False, load_from="expiration-date")
+    instrument = fields.String(required=False, load_from="instrument")
+    migrating_authority = fields.String(required=False, load_from="migrating-authority")
+    old_register_part = fields.String(required=False, load_from="old-register-part")
+    place_of_inspection = fields.String(required=False, load_from="place-of-inspection")
+    inspection_reference = fields.String(required=False, load_from="inspection-reference")
 
 class PlaceOfInspectionSchema(Schema):
-    location = fields.String(required=True, attribute="location")
+    location = fields.String(required=True, load_from="location")
 
 class AuthoritySchema(Schema):
-    name = fields.String(required=True, attribute="name")
+    name = fields.String(required=True, load_from="name")
 
 class ProvisionSchema(Schema):
-    description = fields.String(required=True, attribute="description")
+    description = fields.String(required=True, load_from="description")
