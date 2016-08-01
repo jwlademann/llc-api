161bb117512c61b6ec8198042142eabf2159d436
James Lademann <james.lademann@landregistry.gsi.gov.uk>
2016-08-01 17:20:37 +0100

    Remove superfluous comment block

--- tests/test_views.py
+++ tests/test_views.py
@@ -193,26 +193,3 @@ class TestRegisterEndpoints(unittest.TestCase):
         mock_response.json.return_value = {"some": "json"}
         response = self.app.post('/records/geometry/intersects', data=json.dumps({"some": "json"}), headers={"Host": "local-land-charge.something.gov"})
         self.assertEqual(response.data.decode(), '{"some": "json"}')
-
-#===============================================================================
-# @app.route("/records/geometry/<function>", methods=["POST"])
-# def geometry_search(function):
-#     sub_domain = request.headers['Host'].split('.')[0]
-#     json_payload = request.get_json()
-#     if sub_domain not in register_utils.REGISTER_INFO or not register_utils.REGISTER_INFO[sub_domain]['geometry-search']:
-#         app.logger.warn("Invalid sub-domain '{}' used for geometry search".format(sub_domain))
-#         return (json.dumps({"errors": ['invalid sub-domain']}), 400, {"Content-Type": "application/json"})
-#     result = register_utils.validate_json(sub_domain, '/records/geometry/<function>', request.method, json_payload)
-#     if len(result['errors']) > 0:
-#         app.logger.warn("Error validating geometry search json for sub-domain '{}' error(s) were '{}'".format(sub_domain, str(result['errors'])))
-#         return (json.dumps(result), 400, {"Content-Type": "application/json"})
-#     resolve = request.args.get('resolve')
-#     response = register_utils.register_request(sub_domain, request.path, ["resolve={}".format(resolve)], request.method, json_payload)
-#     if response.status_code != 200:
-#         app.logger.warn("Failure geometry searching for sub-domain '{}' response was '{}'".format(sub_domain, response.text))
-#         return_value = json.dumps({"errors": [response.text]})
-#     else:
-#         app.logger.info("Geometry search completed for sub-domain '{}'".format(sub_domain))
-#         return_value = json.dumps(response.json(), sort_keys=True)
-#     return (return_value, response.status_code, {"Content-Type": "application/json"})
-#===============================================================================
