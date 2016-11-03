7aed162cd1a2f8ff2311df1430a00a0e8c5f3aed
jwlademann <james.lademann@landregistry.gsi.gov.uk>
2016-11-03 16:29:13 +0000

    gegesges

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+set -e
 
 export SETTINGS="application.config.TestConfig"
 
 py.test --junitxml=TEST-llc-api.xml --cov-report term-missing --cov-report xml --cov application tests
+
+aardvark
 
 echo "Running Flake8"
 
